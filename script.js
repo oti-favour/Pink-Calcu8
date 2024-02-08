7afc2c75b0b94e71c2b564cd9360eb35ecba366c
oti-favour <favourgraceoti@gmail.com>
2024-02-08 11:42:10 +0100

    adding functionality to buttons

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".button");
-const specialChars =["%","*", "/", "-", "+", "="]
+const specialChars =["%","*", "/", "-", "+", "="];
+let output = "";
 
 
 const calculate = (btnValue) => {
-
+        if(btnValue === "=" && btnValue !== ""){
+            output = eval(output.replace("%", "/100"));
+        }else if (btnValue === "AC"){
+            output = ""
+        }else if (btnValue === "DEL"){
+            output = output.toString().slice(0,-1);
+        }else{
+            if (output === "" && specialChars.includes(btnValue)) return;
+        }
     display.value= output;
 }
 
