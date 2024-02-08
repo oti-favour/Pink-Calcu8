4a1eb51ea91b6f47f3de839e9ca176be51533afd
oti-favour <favourgraceoti@gmail.com>
2024-02-08 10:48:36 +0100

    adding functionality to buttons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".button");
+const specialChars =["%","*", "/", "-", "+", "="]
+
+
+const calculate = (btnValue) => {
+
+    display.value= output;
+}
 
 button.forEach((button) => {
-    button.addEventListener("click",(e)=>console.log(e.target.dataset.value));
+    button.addEventListener("click",(e)=>calculate(e.target.dataset.value));
 });
